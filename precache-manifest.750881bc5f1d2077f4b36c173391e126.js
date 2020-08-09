77673ddccb3329740a9eea060720ac7bc6be4946
abhi <abhideepak22@gmail.com>
2020-08-09 21:07:12 +0530

    Updates

---
+++ precache-manifest.750881bc5f1d2077f4b36c173391e126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9422b51482eaaaaaaa47009304a2c8cf",
+    "url": "/truckwelcome/index.html"
+  },
+  {
+    "revision": "b55e4bcc96040bf7f7a8",
+    "url": "/truckwelcome/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1952a8224ff744635471",
+    "url": "/truckwelcome/static/css/main.8b076cef.chunk.css"
+  },
+  {
+    "revision": "b55e4bcc96040bf7f7a8",
+    "url": "/truckwelcome/static/js/2.ee436aa6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/truckwelcome/static/js/2.ee436aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1952a8224ff744635471",
+    "url": "/truckwelcome/static/js/main.afc8b467.chunk.js"
+  },
+  {
+    "revision": "b00c9ecbd3068e33e1d1",
+    "url": "/truckwelcome/static/js/runtime-main.85fe2b5d.js"
+  }
+]);
